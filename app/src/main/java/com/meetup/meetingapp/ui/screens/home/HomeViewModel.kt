574b90aab62a115b362a5e9c27dbb7186242e92d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-25 18:29:25 +0200

    Implement Firebase anonymous authentication in HomeViewModel and HomeScreen

--- app/src/main/java/com/meetup/meetingapp/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/meetup/meetingapp/ui/screens/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.meetup.meetingapp.ui.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
 import com.meetup.meetingapp.data.db.entities.ExampleEntity
 import com.meetup.meetingapp.data.repositories.ExampleRepository
 import kotlinx.coroutines.flow.SharingStarted
@@ -14,6 +15,8 @@ import kotlinx.coroutines.flow.stateIn
  */
 class HomeViewModel(private val exampleRepository: ExampleRepository) : ViewModel() {
 
+    private val auth = FirebaseAuth.getInstance()
+
     val homeUiState: StateFlow<HomeUiState> =
         exampleRepository.getAllItems().map { HomeUiState(it) }
             .stateIn(
@@ -21,6 +24,23 @@ class HomeViewModel(private val exampleRepository: ExampleRepository) : ViewMode
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
                 initialValue = HomeUiState()
             )
+
+    /**
+     * Sign in anonymously to the Firebase Realtime Database
+     */
+    fun signInAnonymously() {
+        if (auth.currentUser != null) return
+
+        auth.signInAnonymously()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    println("SUCCESS UID: ${user?.uid}")
+                } else {
+                    println("ERROR: ${task.exception}")
+                }
+            }
+    }
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
