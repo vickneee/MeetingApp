2cc491af225d0deb4a51a2be5848c076c0d27287
Monami <monami.saeki@gmail.com>
2026-04-03 18:10:10 +0300

    Implement participant input submission and integrate Firestore write logic in EventRepository and ParticipantViewModel

--- app/src/main/java/com/meetup/meetingapp/ui/screens/participant_input/SubmissionCompletePage.kt
+++ app/src/main/java/com/meetup/meetingapp/ui/screens/participant_input/SubmissionCompletePage.kt
@@ -1,12 +1,15 @@
 package com.meetup.meetingapp.ui.screens.participant_input
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -16,19 +19,24 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.meetup.meetingapp.MeetingAppTopAppBar
 import com.meetup.meetingapp.R
 import com.meetup.meetingapp.ui.AppViewModelProvider
 import com.meetup.meetingapp.ui.navigation.NavigationDestination
+import com.meetup.meetingapp.ui.screens.event_created_page.ErrorScreen
+import com.meetup.meetingapp.ui.screens.event_created_page.LoadingScreen
 
 object SubmissionCompleteDestination: NavigationDestination {
     override val route = "submission-complete"
@@ -42,12 +50,28 @@ fun SubmissionCompletePage(
     onNavigateToDashboard: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    SubmissionCompleteContent(
-        onBack = onBack,
-        viewModel = viewModel,
-        onNavigateToDashboard = onNavigateToDashboard,
-        modifier = modifier
-    )
+
+    val submitState by viewModel.submitState.collectAsStateWithLifecycle()
+    when(submitState){
+        is SubmitState.Loading -> LoadingScreen(modifier = Modifier.fillMaxSize())
+
+        is SubmitState.Success -> SubmissionCompleteContent(
+            onBack = onBack,
+            viewModel = viewModel,
+            onNavigateToDashboard = onNavigateToDashboard,
+            modifier = modifier
+        )
+
+        is SubmitState.Error -> {
+            val state = submitState as SubmitState.Error
+            ErrorScreen(
+                message = state.error.message ?: "Something went wrong",
+                onRetry = { viewModel.submitParticipantInput() },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+
 
 }
 
@@ -130,6 +154,38 @@ fun SubmissionCompleteContent(
                 }
 
             }
+        }
+    }
+}
+
+/**
+ * The screen displaying the loading message.
+ */
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = "Loading"
+    )
+}
+
+/**
+ * The screen displaying error message with re-attempt button.
+ */
+@Composable
+fun ErrorScreen(message: String, onRetry: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = message, modifier = Modifier.padding(16.dp))
+        Button(onClick = onRetry) {
+            Text("Retry")
         }
     }
 }
