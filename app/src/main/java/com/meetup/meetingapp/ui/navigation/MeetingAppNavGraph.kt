9a3890e058789657480cf3911e795524698df77a
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-04-17 03:14:46 +0300

    Implement navigation for restaurant voting and final plan details

--- app/src/main/java/com/meetup/meetingapp/ui/navigation/MeetingAppNavGraph.kt
+++ app/src/main/java/com/meetup/meetingapp/ui/navigation/MeetingAppNavGraph.kt
@@ -448,6 +448,12 @@ fun MeetingAppNavHost(
                 onNavigateToChooseDatePage = {
                     navController.navigate("${ChooseDateAndAreaDestination.route}/$eventId")
                 },
+                onVoteForRestaurantClick = {
+                    navController.navigate("${ChooseDateAndAreaDestination.route}/$eventId")
+                },
+                onFinalPlanClick = { eventId ->
+                    navController.navigate("${PlaceDetailsDestination.route}/$eventId")
+                },
                 onNavigateToHome = {
                     navController.navigate(HomeDestination.route)
                 }
