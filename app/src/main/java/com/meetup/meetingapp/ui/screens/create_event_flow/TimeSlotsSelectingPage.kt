9ec62aa9f3656d9c00b1dd78c7876b04fce23231
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-04-20 03:18:03 +0300

    Remove unused imports in Create Event flow screens

--- app/src/main/java/com/meetup/meetingapp/ui/screens/create_event_flow/TimeSlotsSelectingPage.kt
+++ app/src/main/java/com/meetup/meetingapp/ui/screens/create_event_flow/TimeSlotsSelectingPage.kt
@@ -35,14 +35,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.google.common.math.LinearTransformation.vertical
 import com.meetup.meetingapp.MeetingAppTopAppBar
 import com.meetup.meetingapp.R
 import com.meetup.meetingapp.data.model.TimeSlot
