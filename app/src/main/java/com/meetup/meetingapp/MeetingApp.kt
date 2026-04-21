9ee4f0189c1573981a436654f03074be5ee143ed
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-04-21 03:07:56 +0300

    Update ArrowBack icon to AutoMirrored version

--- app/src/main/java/com/meetup/meetingapp/MeetingApp.kt
+++ app/src/main/java/com/meetup/meetingapp/MeetingApp.kt
@@ -2,7 +2,9 @@ package com.meetup.meetingapp
 
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -50,7 +52,7 @@ fun MeetingAppTopAppBar(
             if (canNavigateBack) {
                 IconButton(onClick = navigateUp) {
                     Icon(
-                        imageVector = Filled.ArrowBack,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = stringResource(R.string.back_button)
                     )
                 }
